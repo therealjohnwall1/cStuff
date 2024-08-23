2d025a183065bd36921c81944246cf5add2fee0b
therealjohnwall1 <andyvvan@gmail.com>
2024-08-22 22:41:57 -0700

    morestuff

--- fileCompression/huffman.cpp
+++ fileCompression/huffman.cpp
@@ -12,7 +12,7 @@ using std::cout;
 using std::priority_queue;
 using std::vector;
 
-priority_queue<freqNode,vector<freqNode>,compareNode> countFrequencies(string filePath) {
+priority_queue<freqNode,vector<freqNode>,compareNode>* countFrequencies(string filePath) {
     fstream file(filePath, std::ios::in);
     unordered_map<char, int> freqMap; 
     
@@ -45,32 +45,58 @@ priority_queue<freqNode,vector<freqNode>,compareNode> countFrequencies(string fi
     //} 
 	
 	// create frequency table(prior queue)
-     priority_queue<freqNode,vector<freqNode>,compareNode> freqQueue;
+     //priority_queue<freqNode,vector<freqNode>,compareNode> freqQueue;
+     priority_queue<freqNode,vector<freqNode>,compareNode> *freqQueue = new priority_queue<freqNode,vector<freqNode>,compareNode>;
         
      for(auto i: freqMap) {
          // char, freq
          // constructor takes frequency first
         freqNode toIns(i.second, i.first);
-        freqQueue.push(toIns);
+        freqQueue->push(toIns);
      }
+     // need to delete after 
      return freqQueue;
 }
 
-void buildTree(priority_queue<freqNode,vector<freqNode>,compareNode> *queue) {
-    // test queue
+freqNode* buildTree(priority_queue<freqNode,vector<freqNode>,compareNode> *queue) {
+    // while queue length is geq 2
+   //freqNode *root = new freqNode();
+   freqNode *root = (freqNode*)malloc(sizeof(freqNode*));
+    while(queue->size() >=  2) {
+       freqNode *first = new freqNode(queue->top());
+       queue->pop();
+       freqNode *second= new freqNode(queue->top());
+       queue->pop();
 
+       // merge both of them into a new node
+       // counting node no char should represent only frequency of kids
+       freqNode toAdd(first->frequency + second->frequency, NULL);
+       toAdd.left = first;
+       toAdd.right = second;
+       queue->push(toAdd);
+       if (queue->size() == 2) {
+           *root = queue->top();
+       }
+    }
+    return root;
+}
+
+void generateCodes(freqNode* curr, string& str,unordered_map<char, std::string>& huffmanCode) {
+    // gen codes for each char, store in dict we can write this to a file to encode, save the tree to decode
+    //
+    
+    
 }
 
 
-
 int main() {
+    // example prog
     string path = "huffman.cpp";
-    priority_queue<freqNode,vector<freqNode>,compareNode> test = countFrequencies(path);
-    while(!test.empty()) {
-       freqNode n = test.top(); 
-       cout << "node frequency: " << n.frequency << " : " << n.symbol << "\n";
-       test.pop();
-    }
+    priority_queue<freqNode,vector<freqNode>,compareNode> *test = countFrequencies(path);
+    
+    // build tree
+    freqNode* root;
+    root = buildTree(test);
 
 }
 
---
+++ fileCompression/test.cpp
@@ -0,0 +1,11 @@
+#include <bitset>
+#include <iostream>
+
+
+
+
+int main() {
+    std::bitset<4> b3{"0011"}; 
+
+
+}
